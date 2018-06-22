8f10b3873400fee493e589c0ad0a26d890a6ea00
Alberto Romeu <alrocar@cartodb.com>
2018-06-22 06:52:20 +0200

    update deploy script

--- deploy_to_cartoio.sh
+++ deploy_to_cartoio.sh
@@ -7,7 +7,7 @@ BRANCH_NAME="add_cartodbjs_support"
 
 git remote remove dokku
 git remote add dokku dokku@carto.io:$APP_NAME
-ssh dokku@carto.io app:destroy $APP_NAME https:443:5000
+ssh dokku@carto.io apps:destroy $APP_NAME
 git push dokku $BRANCH_NAME:master
 ssh dokku@carto.io certs:add $APP_NAME /home/dokku/tls/server.crt /home/dokku/tls/server.key
 ssh dokku@carto.io proxy:ports-add $APP_NAME https:443:5000
